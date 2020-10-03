741be34edd65fa9c8c97678230edef907bd42def
Abdeali-53 <amody1@my.centennialcollege.ca>
2020-10-02 20:09:13 -0400

    initial commit

---
+++ Basic Calculator Part 1/301085484_Assignment1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  301085484_Assignment1 (FILE NAME)
+//
+//  Created by Abdeali Mody on 2020-09-30. (AUTHOR'S NAME & DATE)
+//  301085484 (STUDENT ID)
+//  INITIAL VERSION
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
